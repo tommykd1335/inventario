8018083973348086912ee317ec70e81e571ae5d6
Tommy <tommykd1335@gmail.com>
2024-12-06 17:34:57 -0400

    Avanzando

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const session = require('express-session')
 const router = require('./routes/routes')
-const path = require('path');
+const path = require('path')
+
 
 
 const app = express()
@@ -27,11 +28,16 @@ app.use((req, res, next)=>{
     res.locals.message = req.session.message
     delete req.session.message
     next()
-})
+}) 
+
 
 app.set('views', path.join(__dirname, './views'));
 app.engine("ejs", require("ejs").__express); 
 app.set('view engine', 'ejs')
+
+//Ver imagenes
+const carpetaUpload = path.join(__dirname, 'src/upload');
+app.use('/upload', express.static(carpetaUpload));
 
 app.use('', router)
 
